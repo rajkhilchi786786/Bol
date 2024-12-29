725993d5ca483fc849615b01b6cb17737d89f6c9
rajkhilchi786786 <sutharshivlal705@gmail.com>
2024-12-29 23:43:58 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY 
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7709792223:AAEy3BwIheJ2ikrhrAsHKlHlTnKUQmZl7hs")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@RajOwner90")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1549748318")
+
+
+
+
+
+
+
+  
